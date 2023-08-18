dcdf4c011838f2e43f04dc55c0b974a0f957219f
Yuliia Barabash <yuliiabarabash@MacBook-Pro-Yuliia.local>
2023-08-18 21:47:10 +0300

    table corrected

--- src/containers/Products/Products.js
+++ src/containers/Products/Products.js
@@ -7,36 +7,36 @@ import Table from "../../components/Table/Table";
 const Products = () => {
   const [products, setProducts] = useState([]);
 
-  useEffect((id) => {
-    fetch(`https://64df853771c3335b2582c0c6.mockapi.io/api/products`, {
-      method: "GET",
-      headers: { "content-type": "application/json" },
-    })
-      .then((response) =>
-      {
+  const API_URL = "https://64df853771c3335b2582c0c6.mockapi.io/api/products";
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(API_URL, {
+          method: "GET",
+          headers: { "content-type": "application/json" },
+        });
+
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        return response.json();
-      })
-      .then((data) => {
-        const productsFormatted = data.map((product) => ({
-          id: product.id,
-          type: product.type,
-          name: product.name,
-          quantity: product.quantity,
-          price: product.price,
-        }));
-        setProducts(productsFormatted);
-      })
-      .catch((error) => console.error("Error fetching data:", error));
+
+        const data = await response.json();
+
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
   }, []);
 
   return (
     <div className="product">
       <header className="product-header">
         <img src={productsLogo} className="products-logo" alt="main logo" />
-        <ProdButton products={products} />
+        <ProdButton />
         <Table products={products} />
       </header>
     </div>
